6ef38f41cb538690ded6111bbd93479095825fa9
Iryna <iryna.shymbor@coderacademy.edu.au>
2021-11-04 12:13:29 +1100

    full CRUD functionality working; form shared file for new and edit in bricks views

--- app/controllers/bricks_controller.rb
+++ app/controllers/bricks_controller.rb
@@ -1,11 +1,52 @@
 class BricksController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
+  before_action :find_brick, only: %i[show edit update destroy]
 
   def index
     @bricks = Brick.all
   end
 
   def show
+  end
+
+  def new
+    @brick = Brick.new
+  end
+
+  def create
+    @brick = current_user.bricks.new(bricks_params)
+
+    if @brick.save
+      redirect_to @brick
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @brick.update(bricks_params)
+      redirect_to @brick
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @brick.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def find_brick
     @brick = Brick.find(params[:id])
+  end
+
+  def bricks_params
+    params.require(:brick).permit(:name, :description, :price, :category_id)
   end
 end
